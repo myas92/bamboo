3284b5017f4eafece4115c05c4dc793c4104297e
javid.ahmadi <ahmadi.stu88@gmail.com>
2024-05-03 00:49:54 +0330

    [improvement]: update refresh api key

--- bamboo_publisher/utils/hotels-content.js
+++ bamboo_publisher/utils/hotels-content.js
@@ -4,13 +4,14 @@ const { generateHash } = require('./generate-hash');
 
 const getHotelsContentByRequest = async ({from, to}) => {
   try {
+    const ApiKey = refresherApiKey()
     let config = {
       method: 'get',
       maxBodyLength: Infinity,
       url: `https://api.test.hotelbeds.com/hotel-content-api/1.0/hotels?fields=all&language=ENG&from=${from}&to=${to}`,
       headers: {
-        'Api-key': process.env.API_KEY,
-        'X-Signature': generateHash(process.env.API_KEY, process.env.API_SECRET),
+        'Api-key': ApiKey,
+        'X-Signature': generateHash(ApiKey, process.env.API_SECRET),
         'Accept': 'application/json',
         'Accept-Encoding': 'gzip'
       },
---
+++ bamboo_publisher/utils/refresher-api-key.js
@@ -0,0 +1,8 @@
+const MAX_USAGE=50;
+let currentUsage=0;
+function refresherApiKey(){
+    if(currentUsage > MAX_USAGE ){
+        // TODO: generate new API_KEY
+    }
+    return 
+}
